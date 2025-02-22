7174815ee03238d3e2c8d6fb1ba5f175fd40c41f
davershayan12 <davershayan13@gmail.com>
2025-02-22 23:33:01 +0500

    .

--- step06catch_all_segment/src/app/docs/[...slug]/page.tsx
+++ step06catch_all_segment/src/app/docs/[...slug]/page.tsx
@@ -1,7 +1,10 @@
-export default function Docs(){
-    return <>
-    <h1>product1</h1>
-    <h1>product1</h1>
-    <h1>product1</h1>
-    </>
+export default async function Docs({params,}:{params: Promise<{slug:string[]}>}){
+    const {slug}=await params
+    if (slug?.length === 2){
+        return <h1>view doc feature{slug[0]} and concept{slug[1]}.</h1>
+    }else if(slug?.length === 3){
+        return <h1>view doc feature{slug[0]} ,concept{slug[1]} and exmaple{slug[2]}. </h1>
+    }else if(slug?.length === 1){
+        return <h1>view doc feature{slug[0]}.</h1>
+    }
 }
