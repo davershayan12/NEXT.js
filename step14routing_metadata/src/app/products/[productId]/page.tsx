9bb53329a80bd6b4857ea4304d3e8087a1a0cd02
davershayan12 <davershayan13@gmail.com>
2025-02-25 10:01:55 +0500

    .

--- step14routing_metadata/src/app/products/[productId]/page.tsx
+++ step14routing_metadata/src/app/products/[productId]/page.tsx
@@ -1,14 +1,36 @@
 import { Metadata } from "next";
-type Props={
-    params:Promise<{productId: string}>;
-}
-export const generatedMetadata=async({params,}:Props):Promise<Metadata>=>{
-const id =(await params).productId
-return {title:`product: ${id}`
-}
-}
-export default async function ProductsDetails({params}:Props){
-    const productId=(await params).productId;
-    return  <h1>product details {productId}</h1>
-    
-}
+type Props = {
+    params: { productId: string };
+  };
+  
+  export const generateMetadata = async ({
+    params,
+  }: Props): Promise<Metadata> => {
+    try {
+      const id = params.productId;
+      console.log("Generating metadata for product ID:", id);
+      const title = await new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+          if (id) {
+            resolve(`iphone ${id}`);
+          } else {
+            reject("Product ID is missing");
+          }
+        }, 100);
+      });
+      return {
+        title: `Product ${title}`,
+      };
+    } catch (error) {
+      console.error("Error generating metadata:", error);
+      return {
+        title: "Product not found",
+      };
+    }
+  };
+  
+  export default function ProductDetails({ params }: Props) {
+    const productId = params.productId;
+    console.log("Rendering product details for product ID:", productId);
+    return <>Details about product {productId}</>;
+  }
